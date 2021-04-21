6f80d3dc08206f82bbf86140d031e16697e1d602
Brienna Klassen <Brienna.Klassen@gmail.com>
2021-04-21 14:13:51 -0700

    Complete up to Chapter 8 of Angular tutorial

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,18 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <p>
-      input-button-unit works!
-      The title is: {{ title }}
-    </p>
+    <input  #inputElementRef
+            [value]="title"
+            (keyup.enter)="changeTitle($event.target.value)">
+    <button (click)="changeTitle(inputElementRef.value)">
+      Save
+    </button>
   `,
   styleUrls: ['./input-button-unit.component.scss']
 })
 export class InputButtonUnitComponent implements OnInit {
-  title = "Hello People!";
+  title = 'Hello World';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeTitle(newTitle: string): void {
+  this.title = newTitle;
+}
 }
