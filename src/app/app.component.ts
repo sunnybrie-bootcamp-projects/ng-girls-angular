9640787599dd92facd9fc8da74f08dc1ad6ee40a
Brienna Klassen <Brienna.Klassen@gmail.com>
2021-04-21 12:13:04 -0700

    Complete Chapter 4 of Angular tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+    
+    <app-input-button-unit></app-input-button-unit>
+  `,
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'todo-list';
 }
---
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-button-unit',
+  template: `
+    <p>
+      input-button-unit works!
+      The title is: {{ title }}
+    </p>
+  `,
+  styleUrls: ['./input-button-unit.component.scss']
+})
+export class InputButtonUnitComponent implements OnInit {
+  title = "Hello People!";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
